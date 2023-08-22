5d4c5b591f1baf4057cbe31f769915429e1fcb80
Kim-Yong-Kyeong <kyk4957@naver.com>
2023-08-23 04:10:48 +0900

    프로젝트 생성

--- src/main/java/com/kyk/FoodWorld/FoodWorldApplication.java
+++ src/main/java/com/kyk/FoodWorld/FoodWorldApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class FoodWorldApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FoodWorldApplication.class, args);
+		SpringApplication.run(FoodWorldApplication.class, args); //
 	}
 
 }
