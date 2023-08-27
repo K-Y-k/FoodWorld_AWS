b008c2d71db4976b5edfa0a7895a3c967dbf06e0
Kim-Yong-Kyeong <kyk4957@naver.com>
2023-08-28 05:28:41 +0900

    배포 자동화 설정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -34,7 +34,7 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 nohup java -jar \
-        -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-real-db.properties \
+        -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-real.properties \
         -Dspring.profile.active=real \
         $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
         # nohup 실행 시 CodeDeploy는 무한 대기한다.
